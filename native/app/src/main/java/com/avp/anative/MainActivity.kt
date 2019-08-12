71e092637ff65bcaf866aa06a7d837aed6cf25da
AnhVietPham <pham.anh.viet.vlth@gmail.com>
2019-08-12 14:32:38 +0700

    Refactor folder

--- native/app/src/main/java/com/avp/anative/MainActivity.kt
+++ native/app/src/main/java/com/avp/anative/MainActivity.kt
@@ -2,11 +2,21 @@ package com.avp.anative
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import io.flutter.facade.Flutter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnOpenFlutter.setOnClickListener {
+            val flutterView = Flutter.createView(this, lifecycle, "r1")
+            val frameLayout =
+                FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            addContentView(flutterView, frameLayout)
+        }
     }
 }
