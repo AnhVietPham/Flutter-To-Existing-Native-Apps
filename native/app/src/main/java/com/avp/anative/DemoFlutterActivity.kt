c9ddb8eaa29c26179d6a85b8a2e21f10717f4aad
AnhVietPham <pham.anh.viet.vlth@gmail.com>
2019-08-14 15:53:22 +0700

    fix error don't pop route

--- native/app/src/main/java/com/avp/anative/DemoFlutterActivity.kt
+++ native/app/src/main/java/com/avp/anative/DemoFlutterActivity.kt
@@ -3,18 +3,24 @@ package com.avp.anative
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.flutter.facade.Flutter
+import io.flutter.view.FlutterView
 
 class DemoFlutterActivity : AppCompatActivity() {
+    private lateinit var flutterView: FlutterView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (intent != null) {
             val route = intent.getStringExtra("route")
             route?.let { it ->
                 if (it.isNotEmpty()) {
-                    val flutterView = Flutter.createView(this, lifecycle, route)
+                    flutterView = Flutter.createView(this, lifecycle, route)
                     setContentView(flutterView)
                 }
             }
         }
+    }
+
+    override fun onBackPressed() {
+        flutterView.popRoute()
     }
 }
